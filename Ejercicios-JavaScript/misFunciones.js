5f562e2d89ec1071b7b6cee86d26c897e35ab12f
simonbf15 <barralesimon@gmail.com>
2023-07-02 14:24:17 -0300

    ejercicio canvas setInterval y dibujar imagen realizado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -272,3 +272,42 @@ let dibujarCuadriculado = () => {
     ctx.stroke();
     ctx.closePath();
 }
+
+
+function dibujarImagen (posX , posY)  {
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var img= new Image();
+
+img.src= "images/auto.png";
+canvas.width=canvas.width;
+
+img.onload=function() {
+    ctx.drawImage(img, posX, posY);
+}
+
+}
+
+x=0;
+dx=2;
+let animarAuto = ()=> {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+    var img= new Image();
+
+    img.src= "images/auto.png";
+
+    img.onload=function() {
+        ctx.drawImage(img, x, 100);
+    }
+
+    if (x>canvas.width){
+        x=0;
+    }
+    x+=dx;
+
+}
