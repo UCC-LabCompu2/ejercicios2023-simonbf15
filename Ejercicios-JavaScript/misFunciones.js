d2fd04e3be944a996c8ba942fb957a8428926680
simonbf15 <barralesimon@gmail.com>
2023-07-01 22:43:28 -0300

    canvas cuadriculado realizado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -228,3 +228,47 @@ let dibujar = () => {
     }
 
 }
+
+
+let dibujarCuadriculado = () => {
+    var canvas= document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+
+    var alturaMax = canvas.height;
+    var anchoMax = canvas.width;
+
+    ctx.beginPath();
+    for( var i=0 ; i < alturaMax;) {
+    ctx.moveTo(0, i);
+    ctx.lineTo(anchoMax, i);
+    ctx.strokeStyle = "#ffffff"
+    ctx.stroke();
+    i=i+20;
+}
+    ctx.closePath();
+
+    ctx.beginPath();
+
+    for( var i=0 ; i < anchoMax ;) {
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, alturaMax);
+    ctx.strokeStyle = "#ffffff"
+    ctx.stroke();
+    i=i+20;
+}
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(0, alturaMax/2);
+    ctx.lineTo(anchoMax,alturaMax/2);
+    ctx.strokeStyle="#af1e1e";
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(anchoMax/2, 0);
+    ctx.lineTo(anchoMax/2,alturaMax);
+    ctx.strokeStyle="#af1e1e";
+    ctx.stroke();
+    ctx.closePath();
+}
